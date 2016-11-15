9c5b378ed4796d1fcc8f3635ef527928a566fe1e
wrighp <wrightcoder@gmail.com>
2016-11-15 18:40:19 -0500

    Three.js implementation

--- ProjectFiles/main.js
+++ ProjectFiles/main.js
@@ -4,15 +4,42 @@ var points;
 var verticesPlane = [];
 var noise = [];
 var indices = [];
+
+var container;
+var renderer;
+var scene;
+var camera;
+var controller;
+var clock;
 window.onload = function init()
 {
+	container = document.getElementById('container');
+	clock = new THREE.Clock();
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	//document.body.appendChild(renderer.domElement);
+	container.appendChild(renderer.domElement);
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(55.0, window.innerWidth / window.innerHeight, 0.5, 30000);
+	camera.position.set(10,10,10);
+	
+	controller = new THREE.FirstPersonControls(camera);
+	controller.movementSpeed = 10;
+	controller.lookSpeed = .1;
+	camera.lookAt(new THREE.Vector3());
+	
+	var geometry = new THREE.BoxGeometry(5,5,5);
+	var material = new THREE.MeshBasicMaterial({color: 0x00ffff});
+	var cube = new THREE.Mesh(geometry,material);
+	scene.add(cube);
+	
     var simplex = new SimplexNoise();
-    var canvas = document.getElementById( "gl-canvas" );
+    //var canvas = document.getElementById( "gl-canvas" );
     
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+   // gl = WebGLUtils.setupWebGL( canvas );
+   // if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl.enable(gl.DEPTH_TEST);
+   // gl.enable(gl.DEPTH_TEST);
 
     var size = 100;
     var scale = 1;
@@ -24,7 +51,7 @@ window.onload = function init()
     {
         for(var j = 0; j< size; j++)
         {
-            verticesPlane[i+j*size] = new vec3(i*width*2-1, j*width*2-1, 0);
+            verticesPlane[i+j*size] = new THREE.Vector3(i*width*2-1, j*width*2-1, 0);
             
             noise[i+j*size] = (simplex.generateNoise(i* noiseScale, j * noiseScale) *.5)+.5;
             if(i> 0 && j> 0)
@@ -43,16 +70,16 @@ window.onload = function init()
     //
     //  Configure WebGL
     //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    //gl.viewport( 0, 0, canvas.width, canvas.height );
+   // gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
 
     //  Load shaders and initialize attribute buffers
 
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    //var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+   // gl.useProgram( program );
     
     // Load the data into the GPU
-
+/*
     var iB = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iB);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
@@ -75,12 +102,26 @@ window.onload = function init()
     gl.enableVertexAttribArray( vPosition );
 
     //gl.lineWidth(10);
-
+*/
+	onWindowResize();
+	window.addEventListener( 'resize', onWindowResize, false );
     render();
 };
+function onWindowResize( event ) {
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+}
 
+var deltaTime;
+function update(){
+	deltaTime = clock.getDelta();
+	controller.update(deltaTime);
+}
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawElements(gl.LINES, indices.length, gl.UNSIGNED_SHORT, 0);
+    //gl.clear( gl.COLOR_BUFFER_BIT );
+    //gl.drawElements(gl.LINES, indices.length, gl.UNSIGNED_SHORT, 0);
+	update();
+	renderer.render(scene,camera);
     requestAnimationFrame(render);
 }
